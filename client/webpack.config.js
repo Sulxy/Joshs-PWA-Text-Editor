c527cb825f2c8e9a0967a7d775825f0353bc6322
Joshua <Joshuahale829@gmail.com>
2024-04-09 11:17:50 -0500

    Believe I have completed the TODOs in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,43 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
+    plugins: [     // Added these lines. 
+      new HtmlWebpackPlugin({
+        template: './index.html',      // These filepaths may not be right. Added these lines. 
+        filename: 'index.html',     // These filepaths may not be right. Added these lines. 
+      }),     // Added these lines. 
+    new WebpackPwaManifest({     // Added these lines. 
+      name: 'J.A.T.E',     // Added these lines. 
+      short_name: 'J.A.T.E',     // Added these lines. 
+      description: 'Just Another Text Editor',     // Added these lines.   Added these lines. 
+      background_color: '#ffffff',     // Added these lines. 
+      crossorigin: 'use-credentials',     // Added these lines. 
+      icons: [{     // Added these lines. 
+        src: path.resolve('src/images/logo.png'), // These filepaths may not be right.  Added these lines. 
+        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+      }]     // Added these lines. 
+    }),     // Added these lines. 
+    new InjectManifest({     // Added these lines. 
+      swSrc: './src-sw.js',     // Added these lines. 
+      swDest: 'service-worker.js',     // Added these lines. 
+    })
     ],
-
     module: {
-      rules: [
-        
+      rules: [     // Added these lines. 
+        {     // Added these lines. 
+          test: /\.css$/,     // Added these lines. 
+          use: ['style-loader', 'css-loader']     // Added these lines. 
+        },     // Added these lines. 
+        {     // Added these lines. 
+          test: /\.js$/,     // Added these lines. 
+          exclude: /node_modules/,     // Added these lines. 
+          use: {     // Added these lines. 
+            loader: 'babel-loader',     // Added these lines. 
+            options: {     // Added these lines. 
+              presets: ['@babel/preset-env']     // Added these lines. 
+            }
+          }
+        }
       ],
     },
   };
