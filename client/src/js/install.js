e90a2a019a508508f26bc48021a3dfd8ac16ef35
Joshua <Joshuahale829@gmail.com>
2024-04-09 14:34:15 -0500

    Updated install.js file. Compelted TODOs.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,26 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    window.deferredPrompt = event;
+    butInstall.removeAttribute('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) return;
+    promptEvent.prompt();
+    const choiceResult = await promptEvent.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+  });
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('J.A.T.E. was installed.');
+});
