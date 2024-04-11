24fbfdbe55c453305b306a94c2beda826cd6aea1
Joshua <Joshuahale829@gmail.com>
2024-04-11 08:06:15 -0500

    Minor changes to logic of EventListeners in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,25 +3,22 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
     window.deferredPrompt = event;
-    butInstall.removeAttribute('hidden', false);
+    butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-    if (!promptEvent) return;
-    promptEvent.prompt();
-    const choiceResult = await promptEvent.userChoice;
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
+    if (!promptEvent) { 
+      return;
     }
+    promptEvent.prompt();
+    window.defferedPrompt = null;
+    butInstall.classList.toggle('hidden', true);
   });
-// TODO: Add an handler for the `appinstalled` event
 
+// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('J.A.T.E. was installed.');
+  window.defferedPrompt = null;
 });
