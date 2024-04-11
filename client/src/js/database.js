588f1dd8bc1192d62cb3501fcbc9ed717214b043
Joshua <Joshuahale829@gmail.com>
2024-04-11 08:01:26 -0500

    updated logic in putDb and getDb functions of database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,21 +14,21 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await initdb();
+  const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put({ content });
-  await tx.done;
-  // console.error('putDb not implemented');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Content saved to database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readonly');
+  const db = await openDB('jate', 'readonly');
   const store = tx.objectStore('jate');
-  return store.getAll();
-  // console.error('getDb not implemented');
+  const request = store.get(1);
+  const result = await request;
+  result ? console.log('Content retrieved from database', result) : console.log('No content found in database');
 };
 
 initdb();
